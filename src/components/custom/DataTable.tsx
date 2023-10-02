fb435ee84b84c2e1e14b6492c2cbb399da89561f
Lia Cicati <LiaCicati@yandex.com>
2023-10-02 19:33:44 +0200

    feat: Updated DataTable component
    - Added the possibility to add buttons in the table cell for certain actions

--- src/components/custom/DataTable.tsx
+++ src/components/custom/DataTable.tsx
@@ -1,83 +1,99 @@
 import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-  } from "@tanstack/react-table"
-  
-  import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/Table"
-  
-  interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[]
-    data: TData[]
-    emptyStateComponent?: React.ReactNode // New prop for empty state
-    id: string;
-  }
-  
-  export function DataTable<TData, TValue>({
-    columns,
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/Table";
+import { Button } from "../ui/Button";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  emptyStateComponent?: React.ReactNode; // New prop for empty state
+  id: string;
+  renderButton?: (rowData: TData) => React.ReactNode;
+  onButtonClick?: (rowData: TData) => () => void;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  id,
+  renderButton,
+  onButtonClick,
+  emptyStateComponent = <span>No results.</span>, // Default to "No results." if not provided
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
     data,
-    id,
-    emptyStateComponent = <span>No results.</span>, // Default to "No results." if not provided
-  }: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-    })
-  
-    return (
-      <div className="rounded-md border">
-        <Table id={id}>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </TableHead>
-                  )
-                })}
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table id={id}>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+              {renderButton && <TableHead>Action</TableHead>}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+                {renderButton && (
+                  <TableCell>
+                    <Button
+                      onClick={
+                        onButtonClick ? onButtonClick(row.original) : undefined
+                      }
+                    >
+                      {renderButton(row.original)}
+                    </Button>
+                  </TableCell>
+                )}
               </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  {emptyStateComponent} {/* Using the passed prop here */}
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-    )
-  }
-  
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                {emptyStateComponent}
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
