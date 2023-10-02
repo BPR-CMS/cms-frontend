45fe568ecebbda951a30ca99fcc2b10b84d3f031
Lia Cicati <LiaCicati@yandex.com>
2023-10-02 19:40:31 +0200

    feat: added resend inv method in EmailI service
    - POST request to resend invitation if it expired

--- src/services/EmailInvitationService.ts
+++ src/services/EmailInvitationService.ts
@@ -13,7 +13,24 @@ if (typeof window !== "undefined") {
 
 export const inviteUser = async (data: User): Promise<User> => {
   try {
-    const response = await axios.post(`${BASE_URL}/send`, data);
+    const response = await axios.post(`${BASE_URL}/send`, data, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const resendInvitation = async (userId: string): Promise<string> => {
+  try {
+    const response = await axios.post(`${BASE_URL}/resend/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
     return response.data;
   } catch (error) {
     throw error;
