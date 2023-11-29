2ed1c9dfbbc72f8d756c5ec3b9991353d4e03e23
Lia Cicati <LiaCicati@yandex.com>
2023-11-29 18:59:38 +0100

    feat: added Post service
    - Add post

---
+++ src/services/PostService.ts
@@ -0,0 +1,33 @@
+import { Post } from "@/models/Post";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:8080/api/v1";
+let TOKEN = "";
+
+if (typeof window !== "undefined") {
+  TOKEN = window.localStorage.getItem("token") || "";
+  console.log(TOKEN)
+  if (!TOKEN) {
+    console.log("Token is missing from local storage");
+  }
+}
+
+export const addPost = async (
+  collectionId: string,
+  postData: Post
+): Promise<Post> => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/posts/${collectionId}`,
+      postData,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
