6a05a747f20ee078ee909d90337fbaa7defc02d6
Lia Cicati <LiaCicati@yandex.com>
2023-11-30 12:15:51 +0100

    feat: get all posts of a specific collection by id
    - Updated PostService to contain method that retrieves posts data of a specific collection

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -6,7 +6,7 @@ let TOKEN = "";
 
 if (typeof window !== "undefined") {
   TOKEN = window.localStorage.getItem("token") || "";
-  console.log(TOKEN)
+  console.log(TOKEN);
   if (!TOKEN) {
     console.log("Token is missing from local storage");
   }
@@ -20,6 +20,24 @@ export const addPost = async (
     const response = await axios.post(
       `${BASE_URL}/posts/${collectionId}`,
       postData,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getPostsByCollectionId = async (
+  collectionId: string
+): Promise<Post[]> => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/posts/${collectionId}`,
       {
         headers: {
           Authorization: `Bearer ${TOKEN}`,
