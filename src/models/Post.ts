d59c12fc13fd865c4fda96bde80706531af41e52
Lia Cicati <LiaCicati@yandex.com>
2023-12-04 19:58:52 +0100

    feat: updated Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,7 +1,7 @@
 import { Attribute } from "./Attribute";
 export interface Post {
-    postId: string;
-    collectionId: string;
-    attributes: Attribute[]
-  }
-  
+  postId: string;
+  collectionId: string;
+  attributes: Attribute[];
+  userId: string;
+}
