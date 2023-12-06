7f8fe77da1d912c75cc946e667a2f8aba80ebdbc
Lia Cicati <LiaCicati@yandex.com>
2023-12-07 00:51:49 +0100

    fix: redeclared variable TOKEN

--- src/services/EmailInvitationService.ts
+++ src/services/EmailInvitationService.ts
@@ -3,7 +3,7 @@ import { User } from "@/models/User";
 
 // const BASE_URL = "http://localhost:8080/api/v1/invitations";
 const BASE_URL = "https://cms-backend-production-0a2c.up.railway.app/api/v1/invitations";
-
+let TOKEN = ""
 if (typeof window !== "undefined") {
   TOKEN = window.localStorage.getItem("token") || "";
   if (!TOKEN) {
