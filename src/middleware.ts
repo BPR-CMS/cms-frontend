646b33044f32145a84df629c58158ff8d6826c7d
Lia Cicati <LiaCicati@yandex.com>
2023-12-09 15:36:24 +0100

    feat: updated middleware to check for user roles

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,17 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const tokenObject = request.cookies.get("token");
 
-  const isAuthenticated = request.cookies.get("token") !== undefined;
+  const token =
+    tokenObject && typeof tokenObject === "object" ? tokenObject.value : null;
 
-  // Allow the requests for the sign-in page or static files
+  // Exclude the unauthorized page from redirect checks
+  if (pathname === "/unauthorized") {
+    return NextResponse.next();
+  }
+
+  // Allow the requests for the sign-in page, sign-up page, static files, or the home page
   if (
     pathname.startsWith("/_next") ||
     pathname === "/sign-in" ||
@@ -16,10 +23,61 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  // If not authenticated, redirect to the sign-in page
-  if (!isAuthenticated) {
+  if (!token) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  return NextResponse.next();
+  // Decode the JWT and check the user role
+  const payload = parseJwt(token);
+  if (payload && payload._userRole) {
+    const userRole = payload._userRole;
+
+    // Admin has access to all routes
+    if (userRole === "ADMIN") {
+      return NextResponse.next();
+    }
+
+    // Editor has restricted access
+    if (userRole === "EDITOR") {
+      const allowedPaths = ["/dashboard", "/content-manager"];
+      const isAllowed = allowedPaths.some((path) => pathname.startsWith(path));
+      if (isAllowed) {
+        return NextResponse.next();
+      }
+    }
+
+    // DEFAULT role has restricted access to only /dashboard
+    if (userRole === "DEFAULT") {
+      const allowedPaths = ["/dashboard"];
+      const isAllowed = allowedPaths.some((path) => pathname.startsWith(path));
+      if (isAllowed) {
+        return NextResponse.next();
+      }
+    }
+
+    // Redirect to the unauthorized page if trying to access other routes
+    return NextResponse.redirect(new URL("/unauthorized", request.url));
+  }
+
+  // Redirect to sign-in if token is invalid or does not have the required payload
+  return NextResponse.redirect(new URL("/sign-in", request.url));
+}
+
+function parseJwt(token: string) {
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const decodedPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(decodedPayload);
+  } catch (e) {
+    console.error("Error decoding token", e);
+    return null;
+  }
 }
